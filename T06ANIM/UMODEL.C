2f530d2f64e3d72cbeb466a5977ca5ac8937eafc
daryasharkova <sharkovadarya@gmail.com>
2015-06-10 17:12:56 +0400

    HE RAN INTO MY KNIFE TEN TIMES.

---
+++ T06ANIM/UMODEL.C
@@ -0,0 +1,101 @@
+/* FILENAME: UMODEL.C
+ * PROGRAMMER: DS6
+ * PURPOSE: Model unit handle module.
+ * LAST UPDATE: 10.06.2015
+ */
+
+#include <stdlib.h>
+#include <string.h> 
+#include "anim.h"
+#include "vec.h"
+#include "render.h"
+
+/* MODEL image type */
+typedef struct tagds6UNIT_MODEL
+{
+  DS6_UNIT_BASE_FIELDS;
+  ds6GOBJ Model;
+} ds6UNIT_MODEL;
+
+/* Animation object initialization function.
+ * ARGUMENTS:
+ *   - self-pointer - animation object itself:
+ *       ds6UNIT_MODEL *Uni;
+ *   - animation context pointer:
+ *       ds6ANIM *Ani;
+ * RETURNS: none.
+ */
+static VOID DS6_AnimUnitInit( ds6UNIT_MODEL *Uni, ds6ANIM *Ani )
+{  
+  DS6_RndGObjLoad(&Uni->Model, "cow.object");
+} /* End of 'DS6_AnimUnitInit' function */
+
+/* Animation object deinitialization function.
+ * ARGUMENTS:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ds6UNIT_MODEL *Uni;
+ *   - animation context pointer:
+ *       ds6ANIM *Ani;
+ * RETURNS: none.
+ */
+static VOID DS6_AnimUnitClose( ds6UNIT_MODEL *Uni, ds6ANIM *Ani )
+{
+} /* End of 'DS6_AnimUnitClose' function */
+
+/* Функция обновления межкадровых параметров объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - self-pointer - animation object itself:
+ *       ds6UNIT_MODEL *Uni;
+ *   - animation context pointer:
+ *       ds6ANIM *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID DS6_AnimUnitResponse( ds6UNIT_MODEL *Uni, ds6ANIM *Ani )
+{
+} /* End of 'DS6_AnimUnitResponse' function */
+
+/* Функция построения объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ds6UNIT_MODEL *Uni;
+ *   - указатель на контекст анимации:
+ *       ds6ANIM *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID DS6_AnimUnitRender( ds6UNIT_MODEL *Uni, ds6ANIM *Ani )
+{  
+  DS6_RndMatrView = MatrView(VecSet(30, 30, 30), VecSet(0, 0, 0), VecSet(0, 1, 0));
+/*  DS6_RndMatrWorld = MatrMulMatr(MatrMulMatr(MatrMulMatr(
+                                 MatrTranslate(Ani->JX * 59, Ani->JY * 88, 0),
+                                 MatrScale(0.1, 0.1, 0.1)), 
+                                 MatrRotateY(30 * Ani->Time + Ani->JR * 180)), 
+                                 MatrTranslate(0, 0, 100 * Ani->JZ));*/
+
+  DS6_RndMatrWorld = MatrMulMatr(MatrRotateZ(30 * sin(Ani->Time * 8)), MatrRotateY(30 * Ani->Time));
+
+  SelectObject(Ani->hDC, GetStockObject(DC_PEN)); 
+  SetDCPenColor(Ani->hDC, RGB(rand() / 255, rand() / 255, rand() / 255));
+  DS6_RndGObjDraw(&Uni->Model); 
+  
+} /* End of 'DS6_AnimUnitRender' function */
+
+/* Функция создания объекта анимации "мяч".
+ * АРГУМЕНТЫ: Нет.
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (ds6UNIT *) указатель на созданный объект анимации.
+ */
+ds6UNIT * DS6_UnitModelCreate( VOID )
+{
+  ds6UNIT_MODEL *Uni;            
+
+  if ((Uni = (VOID *)DS6_AnimUnitCreate(sizeof(ds6UNIT_MODEL))) == NULL)
+    return NULL;
+  /* заполняем поля */
+  Uni->Init = (VOID *)DS6_AnimUnitInit;
+  Uni->Close = (VOID *)DS6_AnimUnitClose;
+  Uni->Response = (VOID *)DS6_AnimUnitResponse;
+  Uni->Render = (VOID *)DS6_AnimUnitRender;
+  return (ds6UNIT *)Uni;
+} /* End of 'DS6_UnitModelCreate' function */
+
+/* END OF 'UMODEL.C' FILE */
